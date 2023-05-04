0056377b3c9bb92b17d4ace2ea4b943e217b2180
Ricardo Andrade <ricardo.krieg@gmail.com>
2023-05-03 21:25:11 -0300

    docker-compose; campaigns crud

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,55 @@
+class CampaignsController < ApplicationController
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def show
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(create_params)
+
+    if @campaign.save
+      redirect_to :campaigns
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+
+    if @campaign.update(update_params)
+      redirect_to @campaign
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:id])
+
+    @campaign.destroy
+
+    redirect_to :campaigns
+  end
+
+  private
+
+  def create_params
+    params.require(:campaign).permit(:title)
+  end
+
+  def update_params
+    params.require(:campaign).permit(:title)
+  end
+end
