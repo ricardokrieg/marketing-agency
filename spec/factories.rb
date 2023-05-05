56a15dd59d1943dd98f37740514214e3a3eabbe8
Ricardo Andrade <ricardo.krieg@gmail.com>
2023-05-05 08:53:50 -0300

    campaign factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     title { Faker::Marketing.buzzwords.capitalize }
     content { Faker::Lorem.paragraph(sentence_count: 10, random_sentences_to_add: 10) }
     targets { Faker::Internet.username(specifier: 10..15) }
+
+    trait :with_multiple_targets do
+      targets do
+        5.times.inject([]) { |ary| ary << Faker::Internet.username(specifier: 10..15) }.join("\n")
+      end
+    end
   end
 end
